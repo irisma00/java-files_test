47af1c8655417f671cb3c91dd02f8b1daaffebd6
Iris Ma <huaiyao@iastate.edu>
2021-11-10 16:15:32 -0600

    minor change

--- language_features_sample/src/module-info.java
+++ language_features_sample/src/module-info.java
@@ -1,7 +1,6 @@
-module language_features_sample {
+open module language_features_sample {
 	exports com.bytestree.math;
 	exports language_features_sample;
 	requires com.bytestree.calculator;
-	opens com.bytestree.example to com.bytestree.example;
 	uses language_features_sample.SwitchExp;
 }

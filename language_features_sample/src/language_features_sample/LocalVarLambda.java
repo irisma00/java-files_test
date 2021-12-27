89992561bc867a51b7660b7d6c2edbf522f2a703
Iris Ma <huaiyao@iastate.edu>
2021-12-27 16:47:55 -0600

    delete all new var examples

--- language_features_sample/src/language_features_sample/LocalVarLambda.java
+++ language_features_sample/src/language_features_sample/LocalVarLambda.java
@@ -5,10 +5,8 @@ import java.util.Arrays;
 public class LocalVarLambda {
 	public static void main(String[] args) {
 		 
-        var arrInteger = new Integer[]{5, 9, 3, 6, 2, 4, 8, 7, 1};
-        long cnt = Arrays.stream(arrInteger).filter(
-                (var a) -> (a != null && a > 5)).count();
-        System.out.println(cnt);
+        
+        System.out.println("cat");
       
     }
 }
--- language_features_sample/src/language_features_sample/var.java
+++
@@ -1,8 +0,0 @@
-package language_features_sample;
-
-public class var {
-	private String name;
-	public var() {
-		name = "blank"; 
-	}
-}

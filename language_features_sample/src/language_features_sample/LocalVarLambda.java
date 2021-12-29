388fe67af4e6aeb1a142dfd0a21bcea971a37c8d
Iris Ma <huaiyao@iastate.edu>
2021-12-28 18:10:40 -0600

    add files back

--- language_features_sample/src/language_features_sample/LocalVarLambda.java
+++ language_features_sample/src/language_features_sample/LocalVarLambda.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public class LocalVarLambda {
 	public static void main(String[] args) {
 		 
-        
-        System.out.println("cat");
+        var arrInteger = new Integer[]{5, 9, 3, 6, 2, 4, 8, 7, 1};
+        long cnt = Arrays.stream(arrInteger).filter(
+                (var a) -> (a != null && a > 5)).count();
+        System.out.println(cnt);
       
     }
 }

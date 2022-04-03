ed3ed7cc805020e8c43626995e537a01e327de6e
Iris Ma <huaiyao@iastate.edu>
2022-04-03 16:14:08 -0500

    minor change

--- text_block_proj/src/text_block_proj/TextBlockTest.java
+++ text_block_proj/src/text_block_proj/TextBlockTest.java
@@ -2,17 +2,11 @@ package text_block_proj;
 
 public class TextBlockTest {
 	public static void main(String[] args) {
-		String message = "'The time has come,' the Walrus said,\n" +
-                "'To talk of many things:\n" +
-                "Of shoes -- and ships -- and sealing-wax --\n" +
-                "Of cabbages -- and kings --\n" +
-                "And why the sea is boiling hot --\n" +
-                "And whether pigs have wings.'\n";
 		
 		String msg = "'The time has come,' the Walrus said," +
                 "'To talk of many things:" +
-                "Of shoes -- and ships -- and sealing-wax --\n" +
-                "Of cabbages -- and kings --\n" +
+                "Of shoes -- and ships -- and sealing-wax --" +
+                "Of cabbages -- and kings --" +
                 "And why the sea is boiling hot --\n" +
                 "And whether pigs have wings.'\n";
 	}
